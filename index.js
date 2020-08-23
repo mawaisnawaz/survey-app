25cda5ba6504a29db71008242a5d1ab4acd6c710
Awais Nawaz <awais00772@gmail.com>
2020-08-24 00:34:51 +0400

    changed message

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+app.get("/", (req, res) => {
+  res.send({ hi: "i changed something" });
 });
 
 const PORT = process.env.PORT || 5000;
